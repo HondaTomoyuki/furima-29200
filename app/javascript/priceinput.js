00710e69cacba280d5fab69ae907c931b9a795c5
tomoyuki-web <67717647+tomoyuki-web@users.noreply.github.com>
2020-09-17 18:48:34 +0900

    商品出品機能実装修正1回目

--- app/javascript/priceinput.js
+++ app/javascript/priceinput.js
@@ -1,23 +1,23 @@
 function priceInput() {
 
-    var price= document.getElementById("item-price")
+    let price= document.getElementById("item-price")
         price.addEventListener("keyup", () => {
   const priceValue = document.getElementById("item-price").value
         
-    var salesCommission = document.getElementById("add-tax-price");
-    var fee = priceValue * 0.1
-    var feeDecimal = Math.floor(fee); //小数点切捨て
-    var numericalValue = feeDecimal
+    let salesCommission = document.getElementById("add-tax-price");
+    let fee = priceValue * 0.1
+    let feeDecimal = Math.floor(fee); //小数点切捨て
+    let numericalValue = feeDecimal
         numericalValue.toLocaleString()
-        var sales = numericalValue.toLocaleString()
+        sales = numericalValue.toLocaleString()
         salesCommission.innerHTML = sales
         
-    var salesProfit = document.getElementById("profit");
-    var profit = priceValue - fee
-    var profitDecimal = Math.floor(profit); //小数点切捨て
-    var numericalProfit = profitDecimal
+    let salesProfit = document.getElementById("profit");
+    let profit = priceValue - fee
+    let profitDecimal = Math.floor(profit); //小数点切捨て
+    let numericalProfit = profitDecimal
         numericalProfit.toLocaleString()
-    var sales = numericalProfit.toLocaleString()
+        sales = numericalProfit.toLocaleString()
         salesProfit.innerHTML = sales
     });
 }
--- spec/requests/delivery_day_request_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe "DeliveryDays", type: :request do
-
-end
