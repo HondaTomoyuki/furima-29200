f4af8b46d5489b97ae46bc791766520d5e9543d7
tomoyuki-web <67717647+tomoyuki-web@users.noreply.github.com>
2020-09-17 21:52:52 +0900

    商品出品機能実装修正２

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,24 +1,24 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :genre
-  
+  belongs_to_active_hash :burden 
+  belongs_to_active_hash :delivery_day
+  belongs_to_active_hash :prefecture 
+  belongs_to_active_hash :Status
+
   has_one_attached :image
   belongs_to :user
 
   with_options presence: true do
     validates :name
     validates :details
-    
+    validates :category_id
+    validates :status_id
+    validates :burden_id
+    validates :delivery_area_id
+    validates :delivery_time_id
+    validates :image
   end
-
-    with_options presence: true do
-      validates :category_id
-      validates :status_id
-      validates :burden_id
-      validates :delivery_area_id
-      validates :delivery_time_id
-      validates :image
-    end
 
     with_options numericality: { other_than: 1 } do
       validates :category_id
