674fe628b77d66400c01712abdde81d9136fd8f1
tomoyuki-web <67717647+tomoyuki-web@users.noreply.github.com>
2020-09-18 15:43:29 +0900

    ヘロク

--- app/models/area.rb
+++ app/models/area.rb
@@ -1,12 +1,12 @@
-# class Area < ApplicationRecord
+class Area < ApplicationRecord
   
-#   extend ActiveHash::Associations::ActiveRecordExtensions
-#   belongs_to_active_hash :prefecture
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
 
-#   #空の投稿を保存できないようにする
-#   validates :title, :text, :prefecture, presence: true
+  #空の投稿を保存できないようにする
+  validates :title, :text, :prefecture, presence: true
 
-#   #ジャンルの選択が「--」の時は保存できないようにする
-#   validates :prefecture_id, numericality: { other_than: 1 } 
+  #ジャンルの選択が「--」の時は保存できないようにする
+  validates :prefecture_id, numericality: { other_than: 1 } 
 
-# end
+end
--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,10 +1,10 @@
-# class Delivery < ApplicationRecord
-#   extend ActiveHash::Associations::ActiveRecordExtensions
-#   belongs_to_active_hash :delivery_day
+class Delivery < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :delivery_day
 
-#   #空の投稿を保存できないようにする
-#   validates :title, :text, :delivery_day, presence: true
+  #空の投稿を保存できないようにする
+  validates :title, :text, :delivery_day, presence: true
 
-#   #ジャンルの選択が「--」の時は保存できないようにする
-#   validates :delivery_day_id, numericality: { other_than: 1 } 
-# end
+  #ジャンルの選択が「--」の時は保存できないようにする
+  validates :delivery_day_id, numericality: { other_than: 1 } 
+end
