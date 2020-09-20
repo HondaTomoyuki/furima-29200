aa74c37df90f5b55a62c04c787941f0c9fa078a8
tomoyuki-web <67717647+tomoyuki-web@users.noreply.github.com>
2020-09-21 07:07:14 +0900

    商品情報編集機能修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @item = Item.includes(:user).order("created_at DESC")
@@ -19,15 +20,12 @@ class ItemsController < ApplicationController
   end 
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
      if @item.update (item_params)
         redirect_to action: :show
      else
@@ -36,6 +34,9 @@ class ItemsController < ApplicationController
   end
    
 
+  def set_item
+    @item = Item.find(params[:id])
+  end
 
   private
 
